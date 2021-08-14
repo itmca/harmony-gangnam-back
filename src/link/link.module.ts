a8fb8bc4ff7ee300f337d4133784856992d0f24e
john <lord1229@gmail.com>
2021-08-14 18:45:50 +0900

    mongodb 추가

--- src/link/link.module.ts
+++ src/link/link.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Link, LinkSchema } from './schemas/link.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature(
+      [{ name: Link.name, schema: LinkSchema }],
+      'link',
+    ),
+  ],
+})
 export class LinkModule {}
---
+++ src/link/link/link.service.spec.ts
@@ -0,0 +1,35 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Link, LinkSchema } from '../schemas/link.schema';
+import { LinkService } from './link.service';
+
+describe('LinkService', () => {
+  let service: LinkService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LinkService],
+      imports: [
+        MongooseModule.forRoot('mongodb://localhost/link', {
+          connectionName: 'link',
+        }),
+        MongooseModule.forFeature(
+          [{ name: Link.name, schema: LinkSchema }],
+          'link',
+        ),
+      ],
+    }).compile();
+
+    service = module.get<LinkService>(LinkService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+
+    service.create({
+      name: 'test',
+    });
+
+    jest.useFakeTimers();
+  });
+});
